c74412f9467865f3e1d596c7e9b29b46e625ee7a
hkb18250157454 <280594803@qq.com>
2020-01-03 16:57:58 +0800

    频率限制

--- config/my.php
+++ config/my.php
@@ -1,9 +1,18 @@
 <?php
 return [
     'sms' => [
-        'sign_name' => env('SIGN_NAME'),
-        'template' => env('TEMPLATE'),
-        'ali_access_key_id'=>env('ALI_ACCESS_KEY_ID'),
-        'ali_access_key_secret'=>env('ALI_ACCESS_KEY_SECRET')
-    ]
+        'sign_name' => env('SIGN_NAME',null),
+        'template' => env('TEMPLATE',null),
+        'ali_access_key_id' => env('ALI_ACCESS_KEY_ID',null),
+        'ali_access_key_secret' => env('ALI_ACCESS_KEY_SECRET',null)
+    ],
+    /*
+   * 接口频率限制
+   */
+    'rate_limits' => [
+        // 访问频率限制，次数/分钟
+        'access' => env('RATE_LIMITS', '60,1'),
+        // 登录相关，次数/分钟
+        'sign' => env('SIGN_RATE_LIMITS', '10,1'),
+    ],
 ];
